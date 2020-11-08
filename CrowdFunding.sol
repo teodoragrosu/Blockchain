fbac276691d3899ae3c54825a326fe634ff8e5f7
oana-hriscu <oana.hriscu97@gmail.com>
2020-11-08 12:43:35 +0200

    Update CrowdFunding.sol

--- CrowdFunding.sol
+++ CrowdFunding.sol
@@ -1,42 +1,57 @@
 pragma solidity >=0.4.22 <0.7.0;
 pragma experimental ABIEncoderV2;
+import 'DistributeFunding.sol';
+import 'OwnerProperty.sol';
 
-contract CrowdFunding {
+contract CrowdFunding is OwnerProperty {
     
-    uint256 fundingGoal;
-    uint256 currentFunds;
+    uint fundingGoal;
+    uint currentFunds;
     bool campaignEnded; //by default initialized with false
-    mapping (uint256 => Contributor) donors;
-    uint256 dIndex;
+    mapping (address => Contributor) donors;
+    DistributeFunding distributeFunding;
     
     struct Contributor{
         string name;
-        uint256 donation;
+        string countryCode;
+        uint donatedSum;
+        bool exists;
     }
     
     event newDonation (Contributor _contributor);
-    event checkStatus (uint256 _currentFunds, string message);
-    event donationRetracted(uint256 _contributorAddress, uint256 _retractedSum);
+    event checkStatus (uint _currentFunds, string message);
+    event donationRetracted(address _contributorAddress, uint _retractedSum);
     event endCampaign ();
    
-    constructor (uint256 _fundingGoal) public {
+    constructor (uint _fundingGoal) public {
         fundingGoal = _fundingGoal;
         currentFunds = 0;
-        dIndex = 0; 
     }
     
-    
-    function donate(Contributor memory _contributor) public {
+    function donate(string memory _name, string memory _countryCode ) payable public {
+        
+        Contributor memory currentContributor = donors[msg.sender];
         
         require(
             campaignEnded != true,
             "Our Campaign has ended but thank you very much for your intent!"
             );
         
-        donors[dIndex] = _contributor;
-        dIndex += 1;
-        currentFunds += _contributor.donation;
-        emit newDonation(_contributor);
+        if(currentContributor.exists == false) {  //new donor - does not exist in the mapping
+            currentContributor = Contributor({
+                name: _name,
+                countryCode: _countryCode,
+                donatedSum: msg.value,
+                exists: true
+            });
+        }
+        else {
+            currentContributor.donatedSum += msg.value;   //if an already existing contributor makes another donation, just increase the sum
+        }
+        
+        currentFunds += msg.value;
+        donors[msg.sender] = currentContributor;
+        emit newDonation(currentContributor);
         
         if(currentFunds > fundingGoal){
             campaignEnded = true;
@@ -44,21 +59,41 @@ contract CrowdFunding {
         }
     }
     
-    function retract (uint256 _contributorAddress) public {
+    function retract (uint amount) public {
+        Contributor memory currentContributor = donors[msg.sender];
+        
+        require( 
+            currentContributor.exists == true,
+            "I'm sorry but it looks like you are not part of our list of contributors."
+            );
+        
         require(
             campaignEnded != true,
             "Sorry but our campaign has ended. You can no longer retract your donation."
             );
         
-        emit donationRetracted(_contributorAddress, donors[_contributorAddress].donation);
-        currentFunds -= donors[_contributorAddress].donation;    
-        delete donors[_contributorAddress];
+        
+        currentFunds -= amount;
+        currentContributor.donatedSum -= amount;
+        
+        if(currentContributor.donatedSum == 0) {
+            delete donors[msg.sender];
+        }
+        
+        emit donationRetracted(msg.sender, amount);
+        
+        msg.sender.transfer(amount);
     }
     
-    function sent2distribute (address payable _distributeFunding) public {
+    function sent2distribute () public onlyOwner {
         
-        _distributeFunding.send(currentFunds);
+        require(
+            campaignEnded == true,
+            "Sorry but you cannot distribute the funds until the campaign has ended."
+            );
         
+        distributeFunding.receiveFunds{value:currentFunds};
+        currentFunds = 0;
     }
     
     function campaignStatus() public {
@@ -69,4 +104,3 @@ contract CrowdFunding {
     }
 
 }
-
