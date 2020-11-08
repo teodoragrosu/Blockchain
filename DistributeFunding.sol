728ba664c2c7ac557fdf8c640ac3f843c0539256
oana-hriscu <oana.hriscu97@gmail.com>
2020-11-08 12:43:59 +0200

    Update DistributeFunding.sol

--- DistributeFunding.sol
+++ DistributeFunding.sol
@@ -1,16 +1,41 @@
 pragma solidity >=0.4.22 <0.7.0;
+import 'OwnerProperty.sol';
 
-contract DistributeFunding {
+contract DistributeFunding is OwnerProperty{
+    address crowdfundingOwner;
+    mapping ( address => uint) shares;
+    address payable[] beneficiaries;
+    uint funds = 0;
+    uint currentSharePercentage = 0;
     
-   struct Beneficiary{
-        string name;
-        uint256 percentage;
+    event fundsReceived(uint _funds);
+    
+    
+    constructor() public {
+        crowdfundingOwner = msg.sender;
+    }
+   
+    function receiveFunds() payable external {
+        funds = msg.value;
+        emit fundsReceived(funds);
+    }
+   
+    function addBeneficiary(address payable _beneficiary, uint _percentage) onlyOwner public{
+        
+        require(_percentage <= 100, "The share goes above 100%. Please pick a smaller value.");
+        require(currentSharePercentage + _percentage <= 100, "The share increases the total share above 100%. Please pick a smaller value.");
+        
+        beneficiaries.push(_beneficiary);
+        shares[_beneficiary] = _percentage;
+        currentSharePercentage += _percentage;
     }
     
-    event fundsReceived(uint256 _funds);
-   
-   function receiveFunds(uint256 _funds) public {
-        emit fundsReceived(_funds);
+    function distributeFunds() onlyOwner public{
+        require (funds > 0, "Not enought funds to distribute");
+        
+        for (uint i = 0; i < beneficiaries.length; i++) {
+            beneficiaries[i].transfer(shares[beneficiaries[i]] * funds/100);
+        }
+        
     }
-   
 }
